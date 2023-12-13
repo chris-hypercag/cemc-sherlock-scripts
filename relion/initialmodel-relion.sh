7cc5a089819cf342c686c2a03812fb35df59fbd2
Chris Haley <clhaley@stanford.edu>
2023-12-12 16:56:56 -0800

    Changed file extensions again to correct default so RELION file browser will show scripts

---
+++ relion/initialmodel-relion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=initialmodel-relion
+#SBATCH --mail-type=end,fail
+#SBATCH --ntasks=XXXmpinodesXXX
+#SBATCH --cpus-per-task=XXXthreadsXXX
+#SBATCH --time=XXXextra1XXX
+#SBATCH --mem-per-cpu=XXXextra2XXX
+#SBATCH --partition=XXXqueueXXX
+#SBATCH --error=XXXerrfileXXX
+#SBATCH --output=XXXoutfileXXX
+
+# On Sherlock the RELION program is located in the biology module. 
+# RELION dependencies such as cuda, mpi, ctffind, motioncorr2 etc. are loaded concurrently with relion/4.0.1.
+# 
+module load biology
+module load relion/4.0.1
+
+# Runs command variable supplied by RELION with srun.
+#
+srun --mem-per-cpu=XXXextra2XXX --gres=none XXXcommandXXX
+
+exit
