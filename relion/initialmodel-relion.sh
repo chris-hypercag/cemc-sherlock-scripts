ee58dafce6be25fe79357a90774306f691abae0e
Chris Haley <clhaley@stanford.edu>
2024-03-15 14:17:41 -0700

    Copied class3D-gpu-relion.sh script to initialmodel-relion.sh script. Changed job name.

--- relion/initialmodel-relion.sh
+++ relion/initialmodel-relion.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=initialmodel-relion
 #SBATCH --mail-type=end,fail
-#SBATCH --ntasks=XXXmpinodesXXX
-#SBATCH --cpus-per-task=XXXthreadsXXX
-#SBATCH --time=XXXextra1XXX
-#SBATCH --mem-per-cpu=XXXextra2XXX
-#SBATCH --partition=XXXqueueXXX
 #SBATCH --error=XXXerrfileXXX
 #SBATCH --output=XXXoutfileXXX
+#SBATCH --partition=XXXqueueXXX
+#SBATCH --ntasks=XXXmpinodesXXX
+#SBATCH --cpus-per-task=XXXthreadsXXX
+#SBATCH --mem-per-cpu=XXXextra2XXX
+#SBATCH --time=XXXextra1XXX
+#SBATCH XXXextra3XXX
+#SBATCH XXXextra4XXX
+#SBATCH XXXextra5XXX
+
+# Create record of Slurm Job ID
+#
+echo "Slurm Job ID: $SLURM_JOB_ID"
 
 # On Sherlock the RELION program is located in the biology module. 
 # RELION dependencies such as cuda, mpi, ctffind, motioncorr2 etc. are loaded concurrently with relion/4.0.1.
@@ -17,6 +24,6 @@ module load relion/4.0.1
 
 # Runs command variable supplied by RELION with srun.
 #
-srun --mem-per-cpu=XXXextra2XXX --gres=none XXXcommandXXX
+mpiexec XXXcommandXXX
 
 exit
